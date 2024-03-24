fec3440071996d2a1010a6a56e6df9a16e6b67ba
Lucas <lucas.ferreirasoares@hotmail.com>
2024-03-24 17:02:14 -0300

    tarefa 1 version 2

--- tarefa 1/classes.cpp
+++ tarefa 1/classes.cpp
@@ -16,7 +16,11 @@ class Vehicle
 
     public:
         //metodos de classe
-        void setTipo(string tipo){ this->tipo = tipo; }
+        void setTipo(string tipo)
+        {
+            for(char& c: tipo) c = tolower(c);
+            if(tipo == "carro" || tipo == "caminhao" || tipo == "camionete") this-> tipo = tipo;
+        }
         string getTipo(){ return this->tipo; }
 
         void setCapacidade(int capacidade) { this->capacidade_carga = capacidade; }
@@ -45,13 +49,13 @@ class Clientes
 
     public:
         //metodos de classe
-        void setNome(int nome){ this->nome = nome; }
+        void setNome(string nome){ this->nome = nome; }
         string getNome(){ return this->nome; }
 
         void setIdade(int idade) { if(idade > 18) this->idade = idade; }
         int getIdade(){ return this->idade; }
 
-        void setEndereco(int endereco){ this->endereco = endereco; }
+        void setEndereco(string endereco){ this->endereco = endereco; }
         string getEndereco(){ return this->endereco; }
 };
 
@@ -59,32 +63,32 @@ class Pedidos
 {
     private:
         //atributos de classe
-        string cliente;
+        Clientes cliente;
         string tipo_transporte;
         string local_coleta;
         string local_entrega;
-        string peso_carga;
-        string volume_carga;
+        int peso_carga;
+        int volume_carga;
 
     public:
         //metodos de classe
-        void setCliente(int cliente){ this->cliente = cliente; }
-        string getCliente(){ return this->cliente; }
+        void setCliente(Clientes cliente){ this->cliente = cliente; }
+        Clientes getCliente(){ return this->cliente; }
 
-        void setTipoTransporte(int transporte){ this->tipo_transporte = transporte; }
+        void setTipoTransporte(string transporte){ this->tipo_transporte = transporte; }
         string getTipoTransporte(){ return this->tipo_transporte; }
 
-        void setLocalColeta(int local_coleta){ this->local_coleta = local_coleta; }
+        void setLocalColeta(string local_coleta){ this->local_coleta = local_coleta; }
         string getLocalColeta(){ return this->local_coleta; }
 
-        void setLocalEntrega(int local_entrega){ this->local_entrega = local_entrega; }
+        void setLocalEntrega(string local_entrega){ this->local_entrega = local_entrega; }
         string getLocalEntrega(){ return this->local_entrega; }
 
         void setPeso(int peso){ this->peso_carga = peso; }
-        string getPeso(){ return this->peso_carga; }
+        int getPeso(){ return this->peso_carga; }
 
         void setVolumeCarga(int volume){ this->volume_carga = volume; }
-        string getVolumeCarga(){ return this->volume_carga; }
+        int getVolumeCarga(){ return this->volume_carga; }
 };
 
 int main()
@@ -95,7 +99,22 @@ int main()
     carro1.setChassi("9BD111060T5002156");
     carro1.setLocalizacao("Florida, EUA");
     carro1.setModelo("Toyota Supra");
-    carro1.setTipo("Carro");
+    carro1.setTipo("CarrO");
+    
 
-    cout << carro1.getModelo();
+    cout << carro1.getModelo() << "\n" << carro1.getTipo();
+
+    Clientes cliente1;
+    cliente1.setEndereco("Rua 123, 321");
+    cliente1.setIdade(38);
+    cliente1.setNome("Glauber");
+
+    Pedidos pedido1;
+    pedido1.setCliente(cliente1);
+    pedido1.setLocalColeta("Rua 123");
+    pedido1.setLocalEntrega("Rua 321");
+    pedido1.setPeso(1345);
+    pedido1.setTipoTransporte("carro");
+    pedido1.setVolumeCarga(234);
+
 }
